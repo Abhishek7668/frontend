0eabc2ea44a3d8f0640219422bdca4788bb9e493
Abhishek7668 <as9765025@gmail.com>
2026-07-20 00:55:57 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
         <Routes>
 
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<login />} />
 
             <Route path="/register" element={<Register />} />
 
